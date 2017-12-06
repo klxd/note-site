00dfaa378da62a15f85725f70052fda561fe529f
peng <peng_s@worksap.co.jp>
2017-12-06 17:24:32 +0800

    finished go-top component

--- src/components/GoTop.js
+++ src/components/GoTop.js
@@ -1,16 +1,48 @@
 import React from 'react'
-import Link from 'gatsby-link'
-
 
 export default class GoTop extends React.Component {
+  constructor() {
+    super();
+    this.scrollTarget = this.getScrollTarget();
+    this.offsetTop = 150;
+    this.state = {};
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.onScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll);
+  }
+
+  getScrollTarget = () => {
+    return document.scrollingElement || document.documentElement;
+  };
+
+  onScroll = () => {
+    this.setState({show: this.scrollTarget.scrollTop > this.offsetTop});
+  };
+
+  onClick = () => {
+    this.scrollUp();
+  };
+
+  scrollUp = (size = 100) => {
+    this.scrollTarget.scrollTop -= size;
+    if (this.scrollTarget.scrollTop > 0) {
+      setTimeout(this.scrollUp, 1000/60);
+    } else {
+      this.scrollTarget.scrollTop = 0;
+    }
+  };
+
   render() {
-    const name = this.props.name;
-    const count = this.props.count || '';
+    let style = this.state.show ? {} : {display: 'none'};
     return (
-      <div className="corner-container">
-        <button className="corner-button" data-tooltip="回到顶部" data-tooltip-position="left"
-                aria-label="回到顶部" type="button">
-          <svg className="Zi Zi--BackToTop" title="回到顶部" fill="currentColor" viewBox="0 0 24 24" width="24" height="24">
+      <div className="corner-container" style={style}>
+        <button className="corner-button" type="button" onClick={this.onClick}>
+          <svg fill="currentColor" viewBox="0 0 24 24" width="24" height="24">
             <path
               d="M16.036 19.59a1 1 0 0 1-.997.995H9.032a.996.996 0 0 1-.997-.996v-7.005H5.03c-1.1
             0-1.36-.633-.578-1.416L11.33 4.29a1.003 1.003 0 0 1 1.412 0l6.878 6.88c.782.78.523
---
+++ src/pages/2017-09-01-about-this-site/index.md
@@ -0,0 +1,10 @@
+---
+title: 关于这个网站
+date: "2017-09-01T22:22:32.169Z"
+path:  "/about-this-site"
+tags:
+    - web
+---
+
+## 值得参考的blog
+<https://imququ.com/>
